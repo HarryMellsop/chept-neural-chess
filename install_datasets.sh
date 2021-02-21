cf644bc9ca7b97b80b741e2e420196cf548f4ce2
Harry Mellsop <harrymellsop@gmail.com>
2021-02-21 10:56:32 -0800

    Setup dataset installation scripts

---
+++ install_datasets.sh
@@ -0,0 +1,34 @@
+echo "Checking Kaggle Install..."
+if ! command -v kaggle &> /dev/null
+then
+	echo "Kaggle is not found"
+	pip install --user kaggle
+else
+	echo "Kaggle is installed"
+fi
+
+echo "Checking Kingbase Dataset... (if this fails, check that GSUtil is installed and authenticated)"
+if [ -e "./data/datasets/kingbase-ftfy.txt" ]
+then
+	echo "Kingbase Dataset Exists"
+else
+	gsutil cp gs://gpt-2-poetry/data/kingbase-ftfy.txt ./data/datasets/kingbase-ftfy.txt
+fi
+
+echo "Checking Kaggle Dataset..."
+
+if [ -e "./data/datasets/35-million-chess-games.zip" ]
+then
+	echo "Kaggle Dataset Downloaded"
+else
+	cd data/datasets 
+	kaggle datasets download milesh1/35-million-chess-games
+	cd ../..
+fi
+
+if [ -e "./data/datasets/all_with_filtered_anotations_since1998.txt" ]
+then
+	echo "Kaggle Dataset Unzipped"
+else
+	unzip ./data/datasets/35-million-chess-games.zip
+fi
