f830d9063e462dcdbf4b8e11e92632984fe88249
Harry Mellsop <harrymellsop@gmail.com>
2021-02-21 12:32:35 -0800

    Reee

---
+++ data/process-dataset.py
@@ -0,0 +1,28 @@
+import tqdm
+
+print("Welcome to ChePT Data Preprocessor")
+
+print("Now processing kingbase-ftfy.txt")
+
+unprocessed_kingbase_lines = open("./data/datasets/kingbase-ftfy.txt", "r").readlines()
+
+processed_kingbase_lines = open("./data/datasets-cleaned/kingbase_cleaned.txt", "w")
+
+line_length = []
+for line in tqdm.tqdm(unprocessed_kingbase_lines):
+    split_line = line.split()
+    output_line = " ".join(split_line[6:]) + "\n"
+    if len(output_line) <= 1024:
+        processed_kingbase_lines.writelines(output_line)
+        line_length.append(len(output_line))
+    
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(line_length)
+
+plt.hist(x, density=True, bins=100)  # density=False would make counts
+plt.ylabel('Probability')
+plt.xlabel('Data')
+plt.show()
