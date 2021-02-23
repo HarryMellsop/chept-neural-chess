cdf85ae5418fea463a7f1a1ae278e98d633dbbc7
alangshur <adlangshur@gmail.com>
2021-02-23 11:00:35 -0800

    yes

--- model.py
+++ model.py
@@ -38,8 +38,8 @@ class Block(nn.Module):
         )
     
     def forward(self, x):
-        x += self.attn(self.ln1(x))
-        x += self.mlp(self.ln2(x))
+        x = x + self.attn(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
         return x
 
 
